69736452d2dbe418fd55fb54dd86fe46eea3d0b3
anmolgoyal1212 <anmolgoyal_ee21b6_59@dtu.ac.in>
2025-06-12 13:23:41 +0530

    setup routes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// health check
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({
+    message: "OK",
+  });
+});
+
+// routes import
+import userRouter from "./routes/user.router.js";
+import documentRouter from "./routes/document.router.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/documents", documentRouter);
+
 export { app };
